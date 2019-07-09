1f2119230297bef974384e2a3086fbd2164205a1
camilohenao1001 <camilohenao03291997@gmail.com>
2019-07-09 13:54:30 -0500

    Actualización Splash
    Actualización Slplash

--- lands/Lands/Lands.Android/MainActivity.cs
+++ lands/Lands/Lands.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Lands.Droid
 {
-    [Activity(Label = "Lands", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Lands", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
